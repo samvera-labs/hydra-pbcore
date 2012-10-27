d37bd255becb26a85d420e90dedb8306a92ec2a1
Adam Wead <amsterdamos@gmail.com>
2012-10-27 16:35:54 -0400

    Defining our indexing rules, with sample solr documents and tests

--- lib/hydra-pbcore/datastream/document.rb
+++ lib/hydra-pbcore/datastream/document.rb
@@ -1,158 +1,208 @@
 module HydraPbcore::Datastream
 class Document < ActiveFedora::NokogiriDatastream
 
-	include HydraPbcore::Methods
+  include HydraPbcore::Methods
 
   set_terminology do |t|
-    t.root(:path=>"pbcoreDescriptionDocument", :xmlns => '', :namespace_prefix=>nil)
+    t.root(:path=>"pbcoreDescriptionDocument")
 
     #
     #  pbcoreDescription fields
     #
-    t.pbc_id(:path=>"pbcoreIdentifier", :namespace_prefix=>nil, :namespace_prefix=>nil, :attributes=>{ :source=>"Rock and Roll Hall of Fame and Museum", :annotation=>"PID" })
-
-    t.main_title(:path=>"pbcoreTitle", :namespace_prefix=>nil, :namespace_prefix=>nil, :attributes=>{ :titleType=>"Main" })
-    t.alternative_title(:path=>"pbcoreTitle", :namespace_prefix=>nil, :namespace_prefix=>nil, :attributes=>{ :titleType=>"Alternative" })
-    t.chapter(:path=>"pbcoreTitle", :namespace_prefix=>nil, :namespace_prefix=>nil, :attributes=>{ :titleType=>"Chapter" })
-    t.episode(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Episode" })
-    t.label(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Label" })
-    t.segment(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Segment" })
-    t.subtitle(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Subtitle" })
-    t.track(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Track" })
-    t.translation(:path=>"pbcoreTitle", :namespace_prefix=>nil, :attributes=>{ :titleType=>"Translation" })
-
-    # This is only to display all subjects
-    t.subjects(:path=>"pbcoreSubject", :namespace_prefix=>nil)
-
-    # Individual subject types defined for entry
-    t.lc_subject(:path=>"pbcoreSubject", :namespace_prefix=>nil, :attributes=>{ :source=>"Library of Congress Subject Headings", :ref=>"http://id.loc.gov/authorities/subjects.html" })
-    t.lc_name(:path=>"pbcoreSubject", :namespace_prefix=>nil, :attributes=>{ :source=>"Library of Congress Name Authority File", :ref=>"http://id.loc.gov/authorities/names" })
-    t.rh_subject(:path=>"pbcoreSubject", :namespace_prefix=>nil, :attributes=>{ :source=>"Rock and Roll Hall of Fame and Museum" })
-
-    t.summary(:path=>"pbcoreDescription", :namespace_prefix=>nil, :attributes=>{ :descriptionType=>"Description",
-      :descriptionTypeSource=>"pbcoreDescription/descriptionType",
-      :descriptionTypeRef=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#description",
-      :annotation=>"Summary"}
+    t.pbc_id(:path=>"pbcoreIdentifier", 
+      :attributes=>{ :source=>"Rock and Roll Hall of Fame and Museum", :annotation=>"PID" }
     )
 
-    t.parts_list(:path=>"pbcoreDescription", :namespace_prefix=>nil, :attributes=>{ :descriptionType=>"Table of Contents",
-      :descriptionTypeSource=>"pbcoreDescription/descriptionType",
-      :descriptionTypeRef=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#table-of-contents",
-      :annotation=>"Parts List" }
+    t.main_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Main" }, :index_as => [:searchable, :displayable])
+    t.alternative_title(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Alternative" },
+      :index_as => [:searchable, :displayable]
+    )
+    t.chapter(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Chapter" }, :index_as => [:searchable, :displayable])
+    t.episode(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Episode" }, :index_as => [:searchable, :displayable])
+    t.label(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Label" }, :index_as => [:searchable, :displayable])
+    t.segment(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Segment" }, :index_as => [:searchable, :displayable])
+    t.subtitle(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Subtitle" }, :index_as => [:searchable, :displayable])
+    t.track(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Track" }, :index_as => [:searchable, :displayable])
+    t.translation(:path=>"pbcoreTitle", :attributes=>{ :titleType=>"Translation" }, 
+      :index_as => [:searchable, :displayable]
+    )
+
+    # This is only to display all subjects
+    t.subject_topic(:path=>"pbcoreSubject", :index_as => [:searchable, :facetable])
+
+    # Individual subject types defined for entry
+    t.lc_subject(:path=>"pbcoreSubject", 
+      :attributes=>{ 
+        :source=>"Library of Congress Subject Headings", 
+        :ref=>"http://id.loc.gov/authorities/subjects.html"
+      },
+      :index_as => [:displayable]
+    )
+    t.lc_name(:path=>"pbcoreSubject",
+      :attributes=>{ :source=>"Library of Congress Name Authority File", :ref=>"http://id.loc.gov/authorities/names" },
+      :index_as => [:displayable]
+    )
+    t.rh_subject(:path=>"pbcoreSubject", 
+      :attributes=>{ :source=>"Rock and Roll Hall of Fame and Museum" },
+      :index_as => [:displayable]
+    )
+
+    t.summary(:path=>"pbcoreDescription", 
+      :attributes=>{ 
+        :descriptionType=>"Description",
+        :descriptionTypeSource=>"pbcoreDescription/descriptionType",
+        :descriptionTypeRef=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#description",
+        :annotation=>"Summary"
+        },
+        :index_as => [:searchable, :displayable]
+    )
+
+    t.parts_list(:path=>"pbcoreDescription", 
+      :attributes=>{ 
+        :descriptionType=>"Table of Contents",
+        :descriptionTypeSource=>"pbcoreDescription/descriptionType",
+        :descriptionTypeRef=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#table-of-contents",
+        :annotation=>"Parts List"
+      },
+      :index_as => [:searchable, :displayable]
     )
 
     # This is only to display all genres
-    t.genres(:path=>"pbcoreGenre", :namespace_prefix=>nil)
+    t.genres(:path=>"pbcoreGenre", :index_as => [:searchable, :facetable])
 
     # Individual genre types defined for entry
-    t.getty_genre(:path=>"pbcoreGenre", :namespace_prefix=>nil, :attributes=>{ :source=>"The Getty Research Institute Art and Architecture Thesaurus", :ref=>"http://www.getty.edu/research/tools/vocabularies/aat/index.html" })
-    t.lc_genre(:path=>"pbcoreGenre", :namespace_prefix=>nil, :attributes=>{ :source=>"Library of Congress Genre/Form Terms", :ref=>"http://id.loc.gov/authorities/genreForms.html" })
-    t.lc_subject_genre(:path=>"pbcoreGenre", :namespace_prefix=>nil, :attributes=>{ :source=>"Library of Congress Subject Headings", :ref=>"http://id.loc.gov/authorities/subjects.html" })
+    t.getty_genre(:path=>"pbcoreGenre", 
+      :attributes=>{ 
+        :source=>"The Getty Research Institute Art and Architecture Thesaurus",
+        :ref=>"http://www.getty.edu/research/tools/vocabularies/aat/index.html"
+      },
+      :index_as => [:displayable] 
+    )
+    t.lc_genre(:path=>"pbcoreGenre",
+      :attributes=>{
+        :source=>"Library of Congress Genre/Form Terms", 
+        :ref=>"http://id.loc.gov/authorities/genreForms.html"
+      },
+      :index_as => [:displayable]
+    )
+    t.lc_subject_genre(:path=>"pbcoreGenre",
+      :attributes=>{
+        :source=>"Library of Congress Subject Headings",
+        :ref=>"http://id.loc.gov/authorities/subjects.html"
+      },
+      :index_as => [:displayable]    
+    )
 
 
     # Series field
-    t.pbcoreRelation(:namespace_prefix=>nil) {
-      t.pbcoreRelationIdentifier(:namespace_prefix=>nil, :attributes=>{ :annotation=>"Event Series" })
-    }
-    t.event_series(:ref=>[:pbcoreRelation, :pbcoreRelationIdentifier])
+    t.pbcoreRelation do
+      t.pbcoreRelationIdentifier(:attributes=>{ :annotation=>"Event Series" })
+    end
+    t.event_series(:ref=>[:pbcoreRelation, :pbcoreRelationIdentifier], :index_as => [:searchable, :displayable])
 
     # Terms for time and place
-    t.pbcore_coverage(:path=>"pbcoreCoverage", :namespace_prefix=>nil) {
-      t.coverage(:path=>"coverage", :namespace_prefix=>nil)
-    }
-    t.spatial(:ref => :pbcore_coverage,
-      :path=>'pbcoreCoverage[coverageType="Spatial"]',
-      :namespace_prefix=>nil
+    t.event_place(:path=>"pbcoreCoverage/coverage", 
+      :attributes => {:annotation=>"Event Place"},
+      :index_as => [:searchable, :displayable]
     )
-    t.temporal(:ref => :pbcore_coverage,
-      :path=>'pbcoreDescriptionDocument/pbcoreCoverage[coverageType="Temporal"]',
-      :namespace_prefix=>nil
+    t.event_date(:path=>"pbcoreCoverage/coverage", 
+      :attributes => {:annotation=>"Event Date"},
+      :index_as => [:searchable, :displayable]
     )
-    t.event_place(:proxy=>[:spatial, :coverage])
-    t.event_date(:proxy=>[:temporal, :coverage])
 
     # Contributor names and roles
-    t.contributor(:path=>"pbcoreContributor", :namespace_prefix=>nil) {
-      t.name_(:path=>"contributor", :namespace_prefix=>nil)
-      t.role_(:path=>"contributorRole", :namespace_prefix=>nil, :attributes=>{ :source=>"MARC relator terms" })
-    }
+    t.contributor(:path=>"pbcoreContributor") do
+      t.name_(:path=>"contributor", :index_as => [:searchable, :facetable])
+      t.role_(:path=>"contributorRole", 
+        :attributes=>{ :source=>"MARC relator terms" },
+        :index_as => [:searchable, :displayable]
+      )
+    end
     t.contributor_name(:proxy=>[:contributor, :name])
     t.contributor_role(:proxy=>[:contributor, :role])
 
     # Publisher names and roles
-    t.publisher(:path=>"pbcorePublisher", :namespace_prefix=>nil) {
-      t.name_(:path=>"publisher", :namespace_prefix=>nil)
-      t.role_(:path=>"publisherRole", :namespace_prefix=>nil, :attributes=>{ :source=>"PBCore publisherRole" })
-    }
-    t.publisher_name(:proxy=>[:publisher, :name])
-    t.publisher_role(:proxy=>[:publisher, :role])
+    t.publisher(:path=>"pbcorePublisher") do
+      t.name_(:path=>"publisher")
+      t.role_(:path=>"publisherRole", :attributes=>{ :source=>"PBCore publisherRole" })
+    end
+    t.publisher_name(:proxy=>[:publisher, :name], :index_as => [:searchable, :facetable])
+    t.publisher_role(:proxy=>[:publisher, :role], :index_as => [:searchable, :displayable])
 
-    t.note(:path=>"pbcoreAnnotation", :namespace_prefix=>nil, :atttributes=>{ :annotationType=>"Notes" })
+    t.note(:path=>"pbcoreAnnotation", :atttributes=>{ :annotationType=>"Notes" })
 
     #
     # pbcoreInstantiation fields for the physical item
     #
-    t.pbcoreInstantiation(:namespace_prefix=>nil) {
-      t.instantiationIdentifier(:namespace_prefix=>nil, :attributes=>{ :annotation=>"Barcode", :source=>"Rock and Roll Hall of Fame and Museum" })
-      t.instantiationDate(:namespace_prefix=>nil, :attributes=>{ :dateType=>"created" })
-      t.instantiationPhysical(:namespace_prefix=>nil, :attributes=>{ :source=>"PBCore instantiationPhysical" })
-      t.instantiationStandard(:namespace_prefix=>nil)
-      t.instantiationLocation(:namespace_prefix=>nil)
-      t.instantiationMediaType(:namespace_prefix=>nil, :attributes=>{ :source=>"PBCore instantiationMediaType" })
-      t.instantiationGenerations(:namespace_prefix=>nil, :attributes=>{ :source=>"PBCore instantiationGenerations" })
-      t.instantiationColors(:namespace_prefix=>nil)
-      t.instantiationLanguage(:namespace_prefix=>nil, :attributes=>{ :source=>"ISO 639.2", :ref=>"http://www.loc.gov/standards/iso639-2/php/code_list.php" })
-      t.instantiationRelation(:namespace_prefix=>nil) {
-        t.arc_collection(:path=>"instantiationRelationIdentifier", :namespace_prefix=>nil, :attributes=>{ :annotation=>"Archival collection" })
-        t.arc_series(:path=>"instantiationRelationIdentifier", :namespace_prefix=>nil, :attributes=>{ :annotation=>"Archival Series" })
-        t.col_number(:path=>"instantiationRelationIdentifier", :namespace_prefix=>nil, :attributes=>{ :annotation=>"Collection Number" })
-        t.acc_number(:path=>"instantiationRelationIdentifier", :namespace_prefix=>nil, :attributes=>{ :annotation=>"Accession Number" })
-      }
-      t.instantiationRights(:namespace_prefix=>nil) {
-        t.rightsSummary(:namespace_prefix=>nil)
-      }
-      t.inst_cond_note(:path=>"instantiationAnnotation", :namespace_prefix=>nil, :attributes=>{ :annotationType=>"Condition Notes" })
-      t.inst_clean_note(:path=>"instantiationAnnotation", :namespace_prefix=>nil, :attributes=>{ :annotationType=>"Cleaning Notes" })
-    }
+    t.pbcoreInstantiation do
+      t.instantiationIdentifier(:attributes=>{ 
+        :annotation=>"Barcode", 
+        :source=>"Rock and Roll Hall of Fame and Museum"
+      })
+      t.instantiationDate(:attributes=>{ :dateType=>"created" })
+      t.instantiationPhysical(:attributes=>{ :source=>"PBCore instantiationPhysical" })
+      t.instantiationStandard
+      t.instantiationLocation
+      t.instantiationMediaType(:attributes=>{ :source=>"PBCore instantiationMediaType" })
+      t.instantiationGenerations(:attributes=>{ :source=>"PBCore instantiationGenerations" })
+      t.instantiationColors
+      t.instantiationLanguage(:attributes=>{ 
+        :source=>"ISO 639.2", 
+        :ref=>"http://www.loc.gov/standards/iso639-2/php/code_list.php"
+      })
+      t.instantiationRelation do
+        t.arc_collection(:path=>"instantiationRelationIdentifier", 
+          :attributes=>{ :annotation=>"Archival collection" },
+          :index_as => [:searchable, :facetable]
+        )
+        t.arc_series(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Archival Series" })
+        t.col_number(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Collection Number" })
+        t.acc_number(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Accession Number" })
+      end
+      t.instantiationRights do
+        t.rightsSummary
+      end
+      t.inst_cond_note(:path=>"instantiationAnnotation",
+        :attributes=>{ :annotationType=>"Condition Notes"},
+        :index_as => [:searchable, :displayable]
+      )
+      t.inst_clean_note(:path=>"instantiationAnnotation", 
+        :attributes=>{ :annotationType=>"Cleaning Notes" },
+        :index_as => [:searchable, :displayable]
+      )
+    end
     # Individual field names:
-    t.creation_date(:proxy=>[:pbcoreInstantiation, :instantiationDate])
-    t.barcode(:proxy=>[:pbcoreInstantiation, :instantiationIdentifier])
-    t.repository(:proxy=>[:pbcoreInstantiation, :instantiationLocation])
-    t.format(:proxy=>[:pbcoreInstantiation, :instantiationPhysical])
-    t.standard(:proxy=>[:pbcoreInstantiation, :instantiationStandard])
-    t.media_type(:proxy=>[:pbcoreInstantiation, :instantiationMediaType])
-    t.generation(:proxy=>[:pbcoreInstantiation, :instantiationGenerations])
-    t.language(:proxy=>[:pbcoreInstantiation, :instantiationLanguage])
-    t.colors(:proxy=>[:pbcoreInstantiation, :instantiationColors])
-    t.archival_collection(:proxy=>[:pbcoreInstantiation, :instantiationRelation, :arc_collection])
-    t.archival_series(:proxy=>[:pbcoreInstantiation, :instantiationRelation, :arc_series])
-    t.collection_number(:proxy=>[:pbcoreInstantiation, :instantiationRelation, :col_number])
-    t.accession_number(:proxy=>[:pbcoreInstantiation, :instantiationRelation, :acc_number])
-    t.usage(:proxy=>[:pbcoreInstantiation, :instantiationRights, :rightsSummary])
+    t.creation_date(:ref=>[:pbcoreInstantiation, :instantiationDate], :index_as => [:searchable, :displayable])
+    t.barcode(:ref=>[:pbcoreInstantiation, :instantiationIdentifier], :index_as => [:searchable, :displayable])
+    t.repository(:ref=>[:pbcoreInstantiation, :instantiationLocation], :index_as => [:searchable, :displayable])
+    t.format(:ref=>[:pbcoreInstantiation, :instantiationPhysical], :index_as => [:searchable, :facetable])
+    t.standard(:ref=>[:pbcoreInstantiation, :instantiationStandard], :index_as => [:searchable, :facetable])
+    t.media_type(:ref=>[:pbcoreInstantiation, :instantiationMediaType], :index_as => [:searchable, :facetable])
+    t.generation(:ref=>[:pbcoreInstantiation, :instantiationGenerations], :index_as => [:searchable, :displayable])
+    t.language(:ref=>[:pbcoreInstantiation, :instantiationLanguage], :index_as => [:searchable, :displayable])
+    t.colors(:ref=>[:pbcoreInstantiation, :instantiationColors], :index_as => [:searchable, :displayable])
+    t.archival_collection(
+      :ref=>[:pbcoreInstantiation, :instantiationRelation, :arc_collection], 
+      :index_as => [:searchable, :facetable]
+    )
+    t.archival_series(
+      :ref=>[:pbcoreInstantiation, :instantiationRelation, :arc_series], 
+      :index_as => [:searchable, :displayable]
+    )
+    t.collection_number(
+      :ref=>[:pbcoreInstantiation, :instantiationRelation, :col_number], 
+      :index_as => [:searchable, :displayable]
+    )
+    t.accession_number(:ref=>[:pbcoreInstantiation, :instantiationRelation, :acc_number],
+      :index_as => [:searchable, :displayable]
+    )
+    t.usage(:ref=>[:pbcoreInstantiation, :instantiationRights, :rightsSummary],
+      :index_as => [:searchable, :displayable]
+    )
+    
+    # Inserted terms
     t.condition_note(:proxy=>[:pbcoreInstantiation, :inst_cond_note])
     t.cleaning_note(:proxy=>[:pbcoreInstantiation, :inst_clean_note])
-
-    #
-    # pbcorePart fields
-    #
-    t.pbcorePart(:namespace_prefix=>nil) {
-      t.pbcoreTitle(:namespace_prefix=>nil, :attributes=>{ :titleType=>"song", :annotation=>"part title" })
-      t.pbcoreIdentifier(:namespace_prefix=>nil, :attributes=>{ :source=>"rock hall", :annotation=>"part number" })
-      t.pbcoreDescription(:namespace_prefix=>nil, :attributes=>{ :descriptionType=>"Description",
-        :descriptionTypesource=>"pbcoreDescription/descriptionType",
-        :ref=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#description" }
-      )
-      t.pbcoreContributor(:namespace_prefix=>nil, :xmlns => '') {
-        t.contributor(:namespace_prefix=>nil, :attributes=>{ :annotation=>"part contributor" })
-        t.contributorRole(:namespace_prefix=>nil, :attributes=>{ :source=>"MARC relator terms" })
-      }
-    }
-    t.part_title(:ref=>[:pbcorePart, :pbcoreTitle])
-    t.part_number(:ref=>[:pbcorePart, :pbcoreIdentifier])
-    t.part_description(:ref=>[:pbcorePart, :pbcoreDescription])
-    t.part_contributor(:ref=>[:pbcorePart, :pbcoreContributor, :contributor])
-    t.part_role(:ref=>[:pbcorePart, :pbcoreContributor, :contributorRole])
-
   end
 
 
