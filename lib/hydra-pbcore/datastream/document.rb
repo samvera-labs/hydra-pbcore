0e4d325f73703ce17414232044ae6f52bb153ee3
Adam Wead <amsterdamos@gmail.com>
2012-11-24 13:30:55 -0500

    Removing instantation from Document

--- lib/hydra-pbcore/datastream/document.rb
+++ lib/hydra-pbcore/datastream/document.rb
@@ -7,9 +7,6 @@ class Document < ActiveFedora::NokogiriDatastream
   set_terminology do |t|
     t.root(:path=>"pbcoreDescriptionDocument")
 
-    #
-    #  pbcoreDescription fields
-    #
     t.pbc_id(:path=>"pbcoreIdentifier", 
       :attributes=>{ :source=>"Rock and Roll Hall of Fame and Museum", :annotation=>"PID" }
     )
@@ -132,78 +129,6 @@ class Document < ActiveFedora::NokogiriDatastream
 
     t.note(:path=>"pbcoreAnnotation", :atttributes=>{ :annotationType=>"Notes" }, :index_as => [:searchable])
 
-    #
-    # pbcoreInstantiation fields for the physical item
-    #
-    t.pbcoreInstantiation do
-      t.instantiationIdentifier(:attributes=>{ 
-        :annotation=>"Barcode", 
-        :source=>"Rock and Roll Hall of Fame and Museum"
-      })
-      t.instantiationDate(:attributes=>{ :dateType=>"created" })
-      t.instantiationPhysical(:attributes=>{ :source=>"PBCore instantiationPhysical" })
-      t.instantiationStandard
-      t.instantiationLocation
-      t.instantiationMediaType(:attributes=>{ :source=>"PBCore instantiationMediaType" })
-      t.instantiationGenerations(:attributes=>{ :source=>"PBCore instantiationGenerations" })
-      t.instantiationColors
-      t.instantiationLanguage(:attributes=>{ 
-        :source=>"ISO 639.2", 
-        :ref=>"http://www.loc.gov/standards/iso639-2/php/code_list.php"
-      })
-      t.instantiationRelation do
-        t.arc_collection(:path=>"instantiationRelationIdentifier", 
-          :attributes=>{ :annotation=>"Archival collection" },
-          :index_as => [:searchable, :facetable]
-        )
-        t.arc_series(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Archival Series" })
-        t.col_number(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Collection Number" })
-        t.acc_number(:path=>"instantiationRelationIdentifier", :attributes=>{ :annotation=>"Accession Number" })
-      end
-      t.instantiationRights do
-        t.rightsSummary
-      end
-      t.inst_cond_note(:path=>"instantiationAnnotation",
-        :attributes=>{ :annotationType=>"Condition Notes"},
-        :index_as => [:searchable, :displayable]
-      )
-      t.inst_clean_note(:path=>"instantiationAnnotation", 
-        :attributes=>{ :annotationType=>"Cleaning Notes" },
-        :index_as => [:searchable, :displayable]
-      )
-    end
-    # Individual field names:
-    t.creation_date(:ref=>[:pbcoreInstantiation, :instantiationDate], :index_as => [:not_searchable, :converted_date, :displayable])
-    t.barcode(:ref=>[:pbcoreInstantiation, :instantiationIdentifier], :index_as => [:searchable, :displayable])
-    t.repository(:ref=>[:pbcoreInstantiation, :instantiationLocation], :index_as => [:searchable, :displayable])
-    t.format(:ref=>[:pbcoreInstantiation, :instantiationPhysical], :index_as => [:searchable, :facetable])
-    t.standard(:ref=>[:pbcoreInstantiation, :instantiationStandard], :index_as => [:searchable, :facetable])
-    t.media_type(:ref=>[:pbcoreInstantiation, :instantiationMediaType], :index_as => [:searchable, :facetable])
-    t.generation(:ref=>[:pbcoreInstantiation, :instantiationGenerations], :index_as => [:searchable, :displayable])
-    t.language(:ref=>[:pbcoreInstantiation, :instantiationLanguage], :index_as => [:searchable, :displayable])
-    t.colors(:ref=>[:pbcoreInstantiation, :instantiationColors], :index_as => [:searchable, :displayable])
-    t.archival_collection(
-      :ref=>[:pbcoreInstantiation, :instantiationRelation, :arc_collection], 
-      :index_as => [:searchable, :facetable]
-    )
-    t.archival_series(
-      :ref=>[:pbcoreInstantiation, :instantiationRelation, :arc_series], 
-      :index_as => [:searchable, :displayable]
-    )
-    t.collection_number(
-      :ref=>[:pbcoreInstantiation, :instantiationRelation, :col_number], 
-      :index_as => [:searchable, :displayable]
-    )
-    t.accession_number(:ref=>[:pbcoreInstantiation, :instantiationRelation, :acc_number],
-      :index_as => [:searchable, :displayable]
-    )
-    t.usage(:ref=>[:pbcoreInstantiation, :instantiationRights, :rightsSummary],
-      :index_as => [:searchable, :displayable]
-    )
-    
-    # Inserted terms
-    t.condition_note(:proxy=>[:pbcoreInstantiation, :inst_cond_note])
-    t.cleaning_note(:proxy=>[:pbcoreInstantiation, :inst_clean_note])
   end
 
   def self.xml_template
@@ -230,6 +155,30 @@ class Document < ActiveFedora::NokogiriDatastream
           }
           xml.pbcoreRelationIdentifier(:annotation=>"Event Series")
         }
+        xml.pbcoreRelation {
+          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
+            xml.text "Is Part Of"
+          }
+          xml.pbcoreRelationIdentifier(:annotation=>"Archival Collection")
+        }
+        xml.pbcoreRelation {
+          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
+            xml.text "Is Part Of"
+          }
+          xml.pbcoreRelationIdentifier(:annotation=>"Archival Series")
+        }
+        xml.pbcoreRelation {
+          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
+            xml.text "Is Part Of"
+          }
+          xml.pbcoreRelationIdentifier(:annotation=>"Collection Number")
+        }
+        xml.pbcoreRelation {
+          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
+            xml.text "Is Part Of"
+          }
+          xml.pbcoreRelationIdentifier(:annotation=>"Accession Number")
+        }
         xml.pbcoreCoverage {
           xml.coverage(:annotation=>"Event Place")
           xml.coverageType {
@@ -242,62 +191,8 @@ class Document < ActiveFedora::NokogiriDatastream
             xml.text "Temporal"
           }
         }
+        xml.pbcoreRightsSummary
         xml.pbcoreAnnotation(:annotationType=>"Notes")
-
-        #
-        # Default physical item
-        #
-        xml.pbcoreInstantiation {
-
-          # Item details
-          xml.instantiationIdentifier(:annotation=>"Barcode", :source=>"Rock and Roll Hall of Fame and Museum")
-          xml.instantiationDate(:dateType=>"created")
-          xml.instantiationPhysical(:source=>"PBCore instantiationPhysical")
-          xml.instantiationStandard
-          xml.instantiationLocation {
-            xml.text "Rock and Roll Hall of Fame and Museum,\n2809 Woodland Ave.,\nCleveland, OH, 44115\n216-515-1956\nlibrary@rockhall.org"
-          }
-          xml.instantiationMediaType(:source=>"PBCore instantiationMediaType") {
-            xml.text "Moving image"
-          }
-          xml.instantiationGenerations(:source=>"PBCore instantiationGenerations") {
-            xml.text "Original"
-          }
-          xml.instantiationColors {
-            xml.text "Color"
-          }
-          xml.instantiationLanguage(:source=>"ISO 639.2", :ref=>"http://www.loc.gov/standards/iso639-2/php/code_list.php") {
-            xml.text "eng"
-          }
-          xml.instantiationRelation {
-            xml.instantiationRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-              xml.text "Is Part Of"
-            }
-            xml.instantiationRelationIdentifier(:annotation=>"Archival Collection")
-          }
-          xml.instantiationRelation {
-            xml.instantiationRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-              xml.text "Is Part Of"
-            }
-            xml.instantiationRelationIdentifier(:annotation=>"Archival Series")
-          }
-          xml.instantiationRelation {
-            xml.instantiationRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-              xml.text "Is Part Of"
-            }
-            xml.instantiationRelationIdentifier(:annotation=>"Collection Number")
-          }
-          xml.instantiationRelation {
-            xml.instantiationRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-              xml.text "Is Part Of"
-            }
-            xml.instantiationRelationIdentifier(:annotation=>"Accession Number")
-          }
-          xml.instantiationRights {
-            xml.rightsSummary
-          }
-
-        }
 
       }
 
