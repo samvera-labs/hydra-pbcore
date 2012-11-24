1d9c4446c214db9b59de6d9a2369fdb436a548e3
Adam Wead <amsterdamos@gmail.com>
2012-11-24 13:26:56 -0500

    New spec_helper methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,18 +4,32 @@ require "debugger"
 require "equivalent-xml"
 
 RSpec.configure do |config|
-
-  # == Mock Framework
-  # Note: we're not mocking... yet.
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  #config.mock_with :rspec
-
   config.color = true
-
 end
 
-def fixture(file) #:nodoc
+def fixture(file)
   File.new(File.join(File.dirname(__FILE__), 'fixtures', file))
+end
+
+def sample(file)
+  File.new(File.join('tmp', file))
+end
+
+def save_template input, filename
+  out = File.new(File.join("tmp", filename), "w")
+  out.write(input.to_s)
+  out.close
+end
+
+def equivalent_xml_files(file)
+  f = Nokogiri::XML(fixture file)
+  s = Nokogiri::XML(sample file)
+  EquivalentXml.equivalent?(
+    f, s, 
+    opts = { :element_order => false, :normalize_whitespace => true }
+  )
+end
+
+def random_string
+  (0...50).map{ ('a'..'z').to_a[rand(26)] }.join
 end
