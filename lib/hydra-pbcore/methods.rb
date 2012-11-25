c189aae7695cd5a496deb1f4e54811837b82274f
Adam Wead <amsterdamos@gmail.com>
2012-11-25 15:27:05 -0500

    HydraPbcore::Methods.remove_node defaults to first node

--- lib/hydra-pbcore/methods.rb
+++ lib/hydra-pbcore/methods.rb
@@ -1,10 +1,8 @@
 module HydraPbcore::Methods
 
-
-  def remove_node(type, index)
+  def remove_node(type, index = 0)
     self.find_by_terms(type.to_sym).slice(index.to_i).remove
     self.dirty = true
   end
-
 
 end
