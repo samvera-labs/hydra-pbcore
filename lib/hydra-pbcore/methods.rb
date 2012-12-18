68f351cf3f1131e51d09b5c1f80b9499c40fcb13
Adam Wead <amsterdamos@gmail.com>
2012-12-18 12:39:51 -0500

    Creating some custom solr fields with local to_solr methods

--- lib/hydra-pbcore/methods.rb
+++ lib/hydra-pbcore/methods.rb
@@ -27,4 +27,19 @@ module HydraPbcore::Methods
     xsd.validate(self.to_pbcore_xml)
   end
 
+  # Returns the 4-digit year from a string
+  def get_year(s)
+    begin
+      return DateTime.parse(s).year.to_s
+    rescue
+      if s.match(/^\d\d\d\d$/)
+        return s.to_s
+      elsif s.match(/^(\d\d\d\d)-\d\d$/)
+        return $1.to_s
+      else
+        return nil
+      end
+    end
+  end
+
 end
