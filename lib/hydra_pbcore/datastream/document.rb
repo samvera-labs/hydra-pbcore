30a8cf30910b523840e6325742f583a8c00e658f
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-20 20:15:32 -0600

    Use relationship templates instead of putting them in the master template. This is a backwards incompatible change

--- lib/hydra_pbcore/datastream/document.rb
+++ lib/hydra_pbcore/datastream/document.rb
@@ -162,36 +162,6 @@ class Document < ActiveFedora::NokogiriDatastream
           :descriptionTypeRef=>"http://pbcore.org/vocabularies/pbcoreDescription/descriptionType#table-of-contents",
           :annotation=>"Parts List"
         )
-        xml.pbcoreRelation {
-          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-            xml.text "Is Part Of"
-          }
-          xml.pbcoreRelationIdentifier(:annotation=>"Event Series")
-        }
-        xml.pbcoreRelation {
-          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-            xml.text "Is Part Of"
-          }
-          xml.pbcoreRelationIdentifier(:annotation=>"Archival Collection")
-        }
-        xml.pbcoreRelation {
-          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-            xml.text "Is Part Of"
-          }
-          xml.pbcoreRelationIdentifier(:annotation=>"Archival Series")
-        }
-        xml.pbcoreRelation {
-          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-            xml.text "Is Part Of"
-          }
-          xml.pbcoreRelationIdentifier(:annotation=>"Collection Number")
-        }
-        xml.pbcoreRelation {
-          xml.pbcoreRelationType(:source=>"PBCore relationType", :ref=>"http://pbcore.org/vocabularies/relationType#is-part-of") {
-            xml.text "Is Part Of"
-          }
-          xml.pbcoreRelationIdentifier(:annotation=>"Accession Number")
-        }
         xml.pbcoreRightsSummary {
           xml.rightsSummary
         }
