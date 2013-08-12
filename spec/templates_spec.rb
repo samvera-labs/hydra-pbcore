d4e911b213f4e03239ebcd5c5004c0ec03945896
Justin Coyne <justin@curationexperts.com>
2013-08-12 16:42:33 -0500

    Should be able to set relationship type

--- spec/templates_spec.rb
+++ spec/templates_spec.rb
@@ -3,32 +3,43 @@ require 'spec_helper'
 describe HydraPbcore::Templates do
   
   before :each do 
-    class TestClass
-
+    class TestClass < ActiveFedora::OmDatastream
       class_attribute :institution, :relator, :address
       self.institution = "Rock and Roll Hall of Fame and Museum"
       self.relator     = "MARC relator terms"
       self.address     = "Rock and Roll Hall of Fame and Museum,\n2809 Woodland Ave.,\nCleveland, OH, 44115\n216-515-1956\nlibrary@rockhall.org"
       
-      def self.define_template arg
-      end
-
       include HydraPbcore::Templates
     end
-    @test = TestClass.new
   end
+  subject { TestClass.new }
+
+  let(:xml) { subject.ng_xml }
 
   describe "#digital_instantiation" do
     it "should return a template for a digital instantiaion" do
-      save_template @test.digital_instantiation, "digital_instantiation_template.xml"
+      save_template subject.digital_instantiation, "digital_instantiation_template.xml"
       tmp_file_should_match_exemplar("digital_instantiation_template.xml")
     end
   end
 
   describe "#physical_instantiation" do
     it "should create a template for physical instantiaions such as tapes" do
-      save_template @test.physical_instantiation, "physical_instantiation_template.xml"
+      save_template subject.physical_instantiation, "physical_instantiation_template.xml"
       tmp_file_should_match_exemplar("physical_instantiation_template.xml")
+    end
+  end
+
+  describe "#relation" do
+    it "should take a two arg constructor" do
+      subject.add_child_node(subject.ng_xml.root, :relation, 'foo', 'bar')
+      xml.xpath('//pbcoreRelation/pbcoreRelationType[@source="PBCore relationType"]').text.should == "Is Part Of"
+      xml.xpath('//pbcoreRelation/pbcoreRelationIdentifier[@annotation="bar"]').text.should == "foo"
+    end
+    it "should take a three arg constructor" do
+      subject.add_child_node(subject.ng_xml.root, :relation, 'foo', 'bar', 'baz')
+      xml.xpath('//pbcoreRelation/pbcoreRelationType[@source="PBCore relationType"]').text.should == "baz"
+      xml.xpath('//pbcoreRelation/pbcoreRelationIdentifier[@annotation="bar"]').text.should == "foo"
     end
   end
 
